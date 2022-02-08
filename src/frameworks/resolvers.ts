9784b929baa5b6fb517ae8661be26146c5df1231
harrytang <harry@powerkernel.com>
2022-02-08 21:54:01 +0200

    chore: refactor

--- src/frameworks/resolvers.ts
+++ src/frameworks/resolvers.ts
@@ -8,13 +8,14 @@ import container from '../config/container';
 import IDENTIFIERS from '../config/identifiers';
 import type { ViewSystemController } from '../domains/system/controllers';
 
-const ViewSystemCtl = container.get<ViewSystemController>(
-  IDENTIFIERS.ViewSystemController
-);
-
 const resolvers = {
   Query: {
-    viewSystem: () => ViewSystemCtl.execute(),
+    viewSystem: () => {
+      const ctl = container.get<ViewSystemController>(
+        IDENTIFIERS.ViewSystemController
+      );
+      return ctl.execute();
+    },
   },
 };
 
